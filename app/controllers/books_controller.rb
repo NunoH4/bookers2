eb6ef93acd4b6996f664c99b4ee30f13d1fd7f4e
NunoH4 <132256457+NunoH4@users.noreply.github.com>
2023-05-21 06:17:13 +0000

    [Fix]投稿編集ボタン修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,15 +5,6 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  # def create
-  #   @book = Book.new(book_params)
-  #   @book.user_id = current_user.id
-  #   @book.save
-  #   flash[:notice] = "You have created book successfully."
-  #   redirect_to books_path
-  # end
-  
-  
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -26,17 +17,6 @@ class BooksController < ApplicationController
       render :index
     end
   end
-  
-  
-  # def edit
-  #   @book = Book.find(params[:id])
-  #   if @book.user_id == current_user.id
-  #     render :edit
-  #   else
-  #     redirect_to books_path
-  #   end
-  # end
-  
   
   def edit
     @book = Book.find(params[:id])
@@ -54,17 +34,6 @@ class BooksController < ApplicationController
     @book_post = Book.new
   end
 
-
-  # def update
-  #   @book = Book.find(params[:id])
-  #   if @book.update(book_params)
-  #     flash[:notice] = "You have updated book successfully."
-  #     redirect_to book_path(@book.id)
-  #   else
-  #     render :edit
-  #   end
-  # end
-  
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -74,7 +43,6 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-  
   
   def destroy
     book = Book.find(params[:id])
